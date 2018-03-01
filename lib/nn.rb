fda5c73770a82fd5151aa6b5e6a279ba1c7899ea
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-03-01 10:50:06 +0000

    Added feed forward implementation

--- lib/nn.rb
+++ lib/nn.rb
@@ -9,7 +9,26 @@ module Nn
       @num_layers = sizes.length
       @sizes = sizes
       @biases = sizes[1..-1].map { |s| NMatrix.random([s, 1]) }
-      @weights = sizes.each_cons(2).map { |a, b| NMatrix.random(a, b) }
+      @weights = sizes.each_cons(2).map { |a, b| NMatrix.random([b, a]) }
+    end
+
+    # Calculates the result of the neural network
+    #
+    # @param input [NMatrix[Numeric]] The network input.
+    # @return [Nmatrix[Numeric]] The network output.
+    def feed_forward(input)
+      @biases.zip(@weights).each do |b, w|
+        pp w: w.shape, input: input.shape
+        input = w.dot(input) + b
+        input = input.map(&method(:sigmoid))
+      end
+      input
+    end
+
+    private
+
+    def sigmoid(num)
+      1.0/(1.0+Math::exp(-num))
     end
   end
 end
