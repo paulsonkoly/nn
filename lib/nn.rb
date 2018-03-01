8586b12a6874906c51af9022750959b92350f2f2
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-03-01 10:09:03 +0000

    Added initialize code to Networks.

--- lib/nn.rb
+++ lib/nn.rb
@@ -1,5 +1,15 @@
 require "nn/version"
 
+require 'nmatrix'
+
 module Nn
-  # Your code goes here...
+  class Network
+    # @param sizes Contains the number of neurons in the respective layers.
+    def initialize(*sizes)
+      @num_layers = sizes.length
+      @sizes = sizes
+      @biases = sizes[1..-1].map { |s| NMatrix.random([s, 1]) }
+      @weights = sizes.each_cons(2).map { |a, b| NMatrix.random(a, b) }
+    end
+  end
 end
